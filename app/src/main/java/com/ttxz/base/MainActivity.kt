5779741db3a67f42b7d9d1cdf4b22718d99196ae
ttxz <ttxz0523@gmail.com>
2026-09-14 11:32:58 +0800

    Fix MainActivity onCreate parameter typo

--- app/src/main/java/com/ttxz/base/MainActivity.kt
+++ app/src/main/java/com/ttxz/base/MainActivity.kt
@@ -16,7 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceed: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
